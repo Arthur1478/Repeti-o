130b09095445f49e814be6457006d8b090516e7f
Arthur1478 <arthurgoncalves147@gmail.com>
2025-04-29 11:22:16 -0300

    slv

---
+++ Atv1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+
+        while (contador <= 1)
+        {
+            Console.WriteLine("sextou novamente");
+            contador = contador + 1;
+        }
+    }
+}
