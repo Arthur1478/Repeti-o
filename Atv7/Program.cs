ce191a1dcb6254f43d29d959470fcef8584310ca
Arthur1478 <arthurgoncalves147@gmail.com>
2025-04-30 09:25:18 -0300

    slv

---
+++ Atv7/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        int contador = 1;
+
+
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador += 1);
+
+        }
+
+
+        contador = 10;
+
+
+        while (contador >= 1)
+        {
+            Console.WriteLine(contador -= 1);
+
+
+        }
+
+    }
+}
